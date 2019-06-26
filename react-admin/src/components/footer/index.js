d1bc9d3f919a49e190f027988975c351a8638f7b
liyan <liyan@anji-plus.com>
2019-06-26 17:49:44 +0800

    代码优化

---
+++ react-admin/src/components/footer/index.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+
+class FooterCustom extends Component{
+  render(){
+    return (
+      <div>
+        Footer
+      </div>
+    )
+  }
+}
+export default FooterCustom;
