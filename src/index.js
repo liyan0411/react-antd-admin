0ca176e01fdf5d485637976e1ef4abd35b46c940
liyan0411 <541267924@qq.com>
2019-08-01 09:44:28 +0800

    代码优化

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import "_a/styles/index.less";
-import '_a/lib/animate.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '_a/styles/index.less'
+import '_a/lib/animate.css'
 // import '_a/styles/antd/index.less';
-import Page from './Page';
+import Page from './Page'
 // 解决create-react-app IE 兼容性问题
-import 'react-app-polyfill/ie9';
-import * as serviceWorker from './serviceWorker';
-
+import 'react-app-polyfill/ie9'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<Page />, document.getElementById('root'))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
