fe056fd4c19b979f39a10369d836b8156963fca1
liyan0411 <541267924@qq.com>
2019-08-19 10:48:06 +0800

    添加下载文件，上传文件 api封装

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,31 +1,56 @@
-// 引入 axios 配置
+// 引入拦截处理后的 axios
 import request from './request'
+import {message} from "antd";
 
 // post 请求
-export function apiPost(url,data = {}) {
+export function apiPost(url, data = {}) {
   return request({
-    url: url,
+    url,
     method: 'post',
-    data: data
+    data
   })
 }
 
 // get 请求
-export function apiGet(url,params = {}) {
+export function apiGet(url, params = {}) {
   return request({
-    url: url,
+    url,
     method: 'get',
-    params: params
+    params
   })
 }
 
-
-// uploadfile 文件下载
-export function apiDl(url,params = {}) {
+//importFile 导入文件
+export function apiUl(url, data = {},file) {
+  // 参数为formData 格式
+  var fd = new FormData()
+  if (data != null && data !== undefined) {
+    for (var i = 0; i < data.length; i++) {
+      fd.append(data[i].name, data[i].value)
+    }
+  }
+  if (file != null) {
+    fd.append('uploadFile', file)
+  } else {
+    message.warning('请选择文件！')
+    return
+  }
   return request({
-    url: url,
+    url,
     method: 'post',
-		responseType: 'blob',
-    params: params
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    },
+    data: fd
+  })
+}
+
+// uploadFile 文件下载
+export function apiDl(url, data = {}) {
+  return request({
+    url,
+    method: 'post',
+    responseType: 'blob',
+    data
   })
 }
