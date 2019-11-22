34d58ddef879906d72c2f94a193ae8684f04394c
liyan0411 <541267924@qq.com>
2019-11-22 14:35:20 +0800

    优化

--- src/pages/home/child1.js
+++ src/pages/home/child1.js
@@ -14,6 +14,9 @@ class HomeChild1 extends Component{
       });
     })
   }
+  componentWillUnmount(){
+    emitter.removeListtener(this.eventEmitter);
+  }
   render(){
     return <div>homeChild1--------{this.state.message}</div>;
   }
